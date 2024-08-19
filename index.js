ce403990ed2a615d562a4b3c7f3c53d914005992
Loic Jeannin <inklo.ca@gmail.com>
2024-08-19 11:50:10 -0400

    the color change and display the RGB

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let color2 = Math.floor(Math.random()*255);
 let color3 = Math.floor(Math.random()*255);
 
 //donnons une couleur en RGB au body
-document.body.style.background = "rgb(140, 233, 222";
+document.body.style.background = `rgb(${color1}, ${color2}, ${color3})`;
 
-document.body.innerHTML = `<h1>RGB (${color1}, ${color2}, ${color3})</h1>`
+//affichons la couleur du body en direct
+document.body.innerHTML = `<h1>RGB : ( ${color1}, ${color2}, ${color3} )</h1>`
